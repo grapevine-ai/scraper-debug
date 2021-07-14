c3b49824e68ad4601cbe89206b702905b3443b11
Anthony <anthony@grapevine.ai>
2021-07-14 10:48:52 +0200

    -change docker image to use nodemon and use volume sharing -add docker compose file -change to use browserless image

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,24 @@
-FROM zenika/alpine-chrome:89-with-puppeteer
+FROM zenika/alpine-chrome:86-with-puppeteer
 USER root
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 
 # Create app directory
 WORKDIR /usr/src/scraper
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
-COPY package*.json ./
+## Install app dependencies
+## A wildcard is used to ensure both package.json AND package-lock.json are copied
+## where available (npm@5+)
+#COPY package*.json ./
+#
+##RUN npm ci --only=production
+## If you are building your code for development
+#RUN npm install
+#
+## Bundle app source
+#COPY . .
 
-RUN npm ci --only=production
-# If you are building your code for development
-# RUN npm install
-
-# Bundle app source
-COPY . .
+# Install nodemon
+RUN npm install -g nodemon
 
 EXPOSE 3040
-CMD [ "node", "app.js" ]
+CMD [ "nodemon", "--inspect=0.0.0.0",  "app.js" ]
--- services/scrape/puppeteer/getPage.js
+++ services/scrape/puppeteer/getPage.js
@@ -6,7 +6,7 @@ module.exports = async function getPage(){
     return new Promise(async (resolve, reject) => {
         let browser;
         if(process.env.NODE_ENV === 'dev'){
-            let args = ['--js-flags=--expose-gc', '--single-process', '--no-zygote', '--no-sandbox', '--ignore-certificate-errors'];
+            let args = ['--js-flags=--expose-gc', '--single-process', '--no-zygote', '--no-sandbox'];
             if(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(process.env.PROXY_IP)){
                 args.push('--proxy-server=' + process.env.PROXY_ADDRESS);
                 logger.info('Proxy - ' + process.env.PROXY_IP, {user: 'admin', path:'getPage'});
@@ -23,29 +23,24 @@ module.exports = async function getPage(){
                 reject({reason: err});
             });
         }else{
-            let args = ['--js-flags=--expose-gc', '--single-process', '--no-zygote', '--no-sandbox', '--headless', '--disable-gpu', '--ignore-certificate-errors', "--disable-dev-shm-usage", "--remote-debugging-port=9222", "--remote-debugging-address=0.0.0.0"];
+            let args = ['--js-flags=--expose-gc', '--single-process', '--no-zygote', '--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--remote-debugging-port=9222', '--remote-debugging-address=0.0.0.0', "--disable-dev-shm-usage", "--remote-debugging-port=9222", "--remote-debugging-address=0.0.0.0"];
             if(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(process.env.PROXY_IP)){
                 args.push('--proxy-server=' + process.env.PROXY_ADDRESS);
                 logger.info('Proxy - ' + process.env.PROXY_IP, {user: 'admin', path:'getPage'});
             }else{
                 logger.info('No proxy', {user: 'admin', path:'getPage'});
             }
-            browser = await puppeteer.launch({
-                bindAddress: "0.0.0.0",
-                args: args
-            });
-
-            /*
-            browser =await puppeteer.launch({
-                headless: true,
-                ignoreHTTPSErrors:true,
-                executablePath: '/usr/bin/chromium-browser',
-                args: args
-            }).catch(err=>{
-                logger.error('Unable to launch browser - ' + JSON.stringify(err), {user: 'admin', path:'getPage'});
-                reject({reason: err});
-            });
-             */
+            browser = await puppeteer.connect({browserWSEndpoint: 'ws://chrome:3000'} );
+            // browser =await puppeteer.launch({
+            //     //dumpio: true,
+            //     headless: true,
+            //     ignoreHTTPSErrors:true,
+            //     executablePath: '/usr/bin/chromium-browser',
+            //     args: args
+            // }).catch(err=>{
+            //     logger.error('Unable to launch browser - ' + JSON.stringify(err), {user: 'admin', path:'getPage'});
+            //     reject({reason: err});
+            // });
         }
         if(browser){
             logger.info('Create page', {user: 'admin', path:'getPage'});
